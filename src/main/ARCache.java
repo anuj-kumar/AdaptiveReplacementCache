88e0c3a2170cd9ca07e60a0c525fec1623c454cd
Anuj Kumar <anujkumar.nitdgp@gmail.com>
2018-09-27 09:17:14 +0530

    Main Adaptive Replacement Cache class

---
+++ src/main/ARCache.java
@@ -0,0 +1,72 @@
+package main;
+
+import exceptions.KeyNotFoundException;
+
+public class ARCache<K, V> implements iCache<K, V> {
+
+	protected AdaptiveLRUCache<K, V> recentCache;
+	protected AdaptiveLFUCache<K, V> frequentCache;
+	protected int size;
+
+	public ARCache(int size) {
+		this.size = size;
+		this.recentCache   = new AdaptiveLRUCache<K, V>(size / 2);
+		this.frequentCache = new AdaptiveLFUCache<K, V>(size - size / 2);
+	}
+	
+	@Override
+	public void set(K key, V value) {
+		if (this.frequentCache.get(key) != null) {
+			this.frequentCache.set(key, value);
+		}
+		this.recentCache.set(key, value);
+	}
+
+	/*
+	 * get an entry from the cache
+	 * Algorithm:
+	 * 	If key exists in LRU cache:
+	 * 		promote it to LFU
+	 *  else if key exists in LFU:
+	 *  	return the value from LFU
+	 *  else if key is a recency ghost:
+	 *  	increase the recency cache size by 1
+	 *  	decrease frequency cache size by 1
+	 *  else if key is a frequency ghost:
+	 *  	increase frequency cache size by 1
+	 *  	decrease recency cache size by 1
+	 * 
+	 */
+	@Override
+	public V get(K key) {
+		V value = null;
+		try {
+			value = this.recentCache.remove(key);
+			this.frequentCache.set(key, value);
+			return value;
+		} catch(KeyNotFoundException e) {
+			value = this.frequentCache.get(key);
+			if (value != null) {
+				return value;
+			}
+
+			if (this.recentCache.isGhost(key)) {
+				int recentCacheSize = this.recentCache.getSize();
+				if (recentCacheSize < this.size) {
+					this.recentCache.expand();
+					this.frequentCache.shrink();
+				}
+			}
+
+			if (this.frequentCache.isGhost(key)) {
+				int frequentCacheSize = frequentCache.getSize();
+				if (frequentCacheSize < this.size) {
+					this.frequentCache.expand();
+					this.recentCache.shrink();
+				}
+			}
+		}
+		return null;
+	}
+
+}
